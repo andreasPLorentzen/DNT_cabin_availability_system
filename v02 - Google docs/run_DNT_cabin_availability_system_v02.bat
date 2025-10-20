b01b7bd4f407d24331866e5a5fe27667c9d92255
Andreas Lorentzen <adlr@cowi.com>
2025-10-20 11:09:50 +0200

    Backup of statistics before new env.

---
+++ v02 - Google docs/run_DNT_cabin_availability_system_v02.bat
@@ -0,0 +1,15 @@
+ECHO OFF
+
+ECHO Running importent stuff
+C:\00_GIT\DNT_cabin_availability_system\venv\Scripts\python.exe "C:/00_GIT/DNT_cabin_availability_system/v02 - Google docs/DNT_cabin_availability_v02_google_docs.py"
+
+
+ECHO Running AGOL stuff
+"C:\Program Files\ArcGIS\Pro\bin\Python\envs\arcgispro-py3\python.exe" "C:/00_GIT/DNT_cabin_availability_system/v02 - Google docs/Update_AGOL.py"
+
+ECHO Sucsess!
+
+
+ECHO ON
+
+timeout 600
---
+++ v02 - Google docs/run_DNT_cabin_availability_system_v02_AGOL.bat
@@ -0,0 +1,7 @@
+ECHO OFF
+ECHO Running AGOL stuff
+"C:\Program Files\ArcGIS\Pro\bin\Python\envs\arcgispro-py3\python.exe" "C:/00_GIT/DNT_cabin_availability_system/v02 - Google docs/Update_AGOL.py"
+
+ECHO ON
+
+Pause
